0e3730d3e9bd8889ed7fccb3502d6d1ef00975f8
Ashees20 <106926019+Ashees20@users.noreply.github.com>
2023-11-21 10:09:36 +0530

    Add files via upload

---
+++ app.js
@@ -0,0 +1,5 @@
+const Web3 = require("web3");
+const YOUR_ALCHEMY_API_KEY = "W7jl6p62a3nSW8UWtyTF8YvJyfZkbN8D"; // Make sure to use quotes for the API key
+const web3 = new Web3("wss://eth-goerli.g.alchemy.com/v2/" + YOUR_ALCHEMY_API_KEY);
+
+// Interaction with the smart contract goes here
